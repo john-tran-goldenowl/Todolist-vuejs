bf3e5b1f2a7214cb839c928083c1540f1d97b723
Nghiattps13085 <john.tran.goldenowl@gmail.com>
2022-01-24 12:26:46 +0700

    ToDoList Vuejs
    upload

---
+++ src/components/add-TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+      <div class="text-box">
+            <input v-model="task" type="text"  id="task" placeholder="TASK">
+            <button id="addtolist" @click="addToList">ADD</button>
+        </div>
+</template>
+<script>
+export default {
+    name: 'addTodolist',
+    props: {
+        items:Object
+    },
+    data(){
+        return{
+            item :[],
+            task:null, 
+            id:null,
+            done:false 
+        }
+    },
+    methods:{
+        addToList(){
+            this.id = this.items[this.items.length-1]._id + 1;
+            this.item= {
+                '_id' : this.id,
+                'task' : this.task,
+                'done' : this.done
+            }
+            this.$emit('addtoToDoList',this.item)
+            this.task = "";
+        }
+
+    }
+}
+
+</script>
---
+++ src/components/item-ToDoList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="item" :class="{'done' : content.done, 'pending': !content.done}">
+            <div class="absolue">
+                <i v-if="content.done"  class="fas fa-times-circle" @click="Check"></i>
+                <i v-else class="fas fa-check-circle" @click="Check"></i>
+                <i  class="fas fa-trash" @click="Delete"></i>
+            </div>
+            <h3>{{content.task}}</h3>
+            <p v-if="content.done">done</p>
+            <p v-else>pending</p>
+        </div>
+</template>
+<script>
+export default {
+    name : 'itemTodolist',
+    props:  {
+        content : Object    
+    },
+    methods: {
+        Check(){
+            this.$emit('updateCheck')
+        },
+        Delete(){
+            this.$emit('updateDelete')
+        }
+    }
+   
+}
+</script>
